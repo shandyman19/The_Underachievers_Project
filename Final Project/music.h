d0db92d2fdaaed6b5998103a314b36c4aff59342
nathanam <33728984+nathanam@users.noreply.github.com>
2017-11-17 10:45:47 -0600

    Add files via upload

---
+++ Final Project/music.h
@@ -0,0 +1,9 @@
+#define RICK_ROLL			0
+#define IMERPIAL_MARCH 		1
+#define MARIO_UNDERWORLD	3
+#define MARIO_UNDERWATER	7
+
+
+//#warning "Make sure the open interface has been initialized before calling load_songs()." // you may delete this warning
+/// Loads some songs over the open interface
+void load_songs();
