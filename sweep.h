6c960befc395ee637b417a70c372a1b68ee57f85
shandyman19 <shanders@iastate.edu>
2017-12-01 15:57:33 -0600

    Add files via upload
    Added a new way to detect the skinny towers using an equation

---
+++ sweep.h
@@ -0,0 +1,22 @@
+/*
+ * sweep.h
+ *
+ *  Created on: Nov 12, 2017
+ *      Author: shanders
+ */
+
+#ifndef SWEEP_H_
+#define SWEEP_H_
+
+int sweep(void);
+
+int distResults(int value);
+
+int posResults(int value);
+
+int smallPosResult(int value);
+
+int smallAngleResult(int value);
+
+//int shortWid();
+#endif /* SWEEP_H_ */
